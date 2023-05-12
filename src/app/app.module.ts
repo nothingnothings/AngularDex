539df3f36ce934f06480e652cd5e1c42c0b43107
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-12 11:15:41 -0300

    fixed router links in header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/UI/toolbar/toolbar.component';
 import { MainNavigationComponent } from './components/UI/toolbar/mainNavigation/mainNavigation.component';
 import { NavigationItemsComponent } from './components/UI/toolbar/mainNavigation/navigationItems/navigationItems.component';
-import { NavigationItemComponent } from './components/UI/toolbar/mainNavigation/navigationItems/navigationItem/navigationItem.component';
 import { PokeBallComponent } from './components/UI/pokeBall/pokeBall.component';
 import { SpinnerComponent } from './components/UI/spinner/spinner.component';
 import { ErrorComponent } from './components/errorComponent/errorComponent.component';
