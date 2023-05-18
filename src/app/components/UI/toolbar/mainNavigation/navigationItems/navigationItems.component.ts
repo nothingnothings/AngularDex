9491ccb64aa489d048a896e7ef8658b6c626c987
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-17 21:54:37 -0300

    attempt to redeploy

--- src/app/components/UI/toolbar/mainNavigation/navigationItems/navigationItems.component.ts
+++ src/app/components/UI/toolbar/mainNavigation/navigationItems/navigationItems.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NavItem } from 'src/app/types/navItem.model';
 
 @Component({
-  templateUrl: './NavigationItems.component.html',
-  styleUrls: ['./NavigationItems.component.scss'],
+  templateUrl: './navigationItems.component.html',
+  styleUrls: ['./navigationItems.component.scss'],
   selector: 'app-navigation-items',
 })
 export class NavigationItemsComponent implements OnInit {
