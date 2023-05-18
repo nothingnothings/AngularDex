f919ccee1dc0260841710dda3468c577b3185dff
Arthur Panazolo <arthur.panazolo@acad.pucrs.br>
2023-05-17 23:07:38 -0300

    removed unecessary parameters in pokedex-resolver

--- src/app/resolvers/pokedex-resolver.service.ts
+++ src/app/resolvers/pokedex-resolver.service.ts
@@ -1,20 +1,16 @@
 import { Injectable } from '@angular/core';
 
-import {
-  ActivatedRouteSnapshot,
-  Resolve,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { Resolve } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PokedexService } from 'src/app/services/pokedex.service';
 import { SimplePokemon } from '../types/simplePokemon.model';
 
 @Injectable({ providedIn: 'root' })
 export class PokedexResolverService implements Resolve<SimplePokemon[]> {
-  resolve(
-    _route: ActivatedRouteSnapshot,
-    _state: RouterStateSnapshot
-  ): SimplePokemon[] | Observable<SimplePokemon[]> | Promise<SimplePokemon[]> {
+  resolve():
+    | SimplePokemon[]
+    | Observable<SimplePokemon[]>
+    | Promise<SimplePokemon[]> {
     const pokemons = this.pokedexService.getPokemons();
 
     if (pokemons.length === 0) {
